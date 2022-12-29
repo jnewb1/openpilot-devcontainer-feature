79ca5c49b00b0fd1eae9bbdc6d648591b47c41bc
Justin Newberry <jnewberry0502@gmail.com>
2022-12-29 15:26:51 +0000

    w

--- src/openpilot/install.sh
+++ src/openpilot/install.sh
@@ -2,7 +2,5 @@
 
 set -x
 
-cp tools/ubuntu_setup.sh /tmp/ubuntu_setup.sh
-
-#chmod +x tools/ubuntu_setup.sh
-#sudo -H -u $_REMOTE_USER bash -c 'bash tools/ubuntu_setup.sh' 
+chmod +xr tools/ubuntu_setup.sh
+sudo -H -u $_REMOTE_USER ./tools/ubuntu_setup.sh
